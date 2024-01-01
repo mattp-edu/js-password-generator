bf0e24b37376039a6397343a3a9f120373b0e286
Matt Panyor <matt@panyor.com>
2024-01-01 00:15:17 +0000

    merge

--- script/script.js
+++ script/script.js
@@ -95,12 +95,40 @@ function getPasswordOptions() {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+<<<<<<< Updated upstream
 
 }
 
 // Function to generate password with user input
 function generatePassword() {
 
+=======
+  // failsafe in case empty array
+  if (arr.length < 1 ) return;
+
+  return arr[Math.round((Math.random() * (arr.length - 1)))];
+}
+
+// Function to generate password with user input
+function generatePassword(settings) {
+  if ( !(8 <= settings.len <= 128) ) return 101;          // check if password in 8-128 letter range
+  if ( !(settings.lower || settings.upper) ) return 102;  // check if at least one type of character has been selected
+  
+  // building up generation array based on settings
+  var gen = [];
+  if (settings.lower) gen = gen.concat(lowerCasedCharacters);
+  if (settings.upper) gen = gen.concat(upperCasedCharacters);
+  if (settings.num) gen = gen.concat(numericCharacters);
+  if (settings.spec) gen = gen.concat(specialCharacters);
+
+  //initialize passphrase variable and iterate on given range to get random letters
+  var passphrase = "";
+  for (let index = 0; index < settings.len; index++) {
+    passphrase = passphrase.concat(getRandom(gen));
+  }
+
+  return passphrase;
+>>>>>>> Stashed changes
 }
 
 // Get references to the #generate element
