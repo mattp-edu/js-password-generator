4e03e29020b8d4c1789dc71d8089cbe475eb5c6f
Matt Panyor <matt@panyor.com>
2024-01-01 00:02:53 +0000

    add random generator based on settings

--- script/script.js
+++ script/script.js
@@ -95,12 +95,29 @@ function getPasswordOptions() {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+  if (arr.length < 1 ) return;
+  return arr[Math.round((Math.random() * (arr.length - 1)))];
 }
 
 // Function to generate password with user input
-function generatePassword() {
+function generatePassword(settings) {
+  if ( !(8 <= settings.len <= 128) ) return 101;          // check if password in 8-128 letter range
+  if ( !(settings.lower || settings.upper) ) return 102;  // check if at least one type of character has been selected
+  
+  var gen = [];
 
+  if (settings.lower) gen = gen.concat(lowerCasedCharacters);
+  if (settings.upper) gen = gen.concat(upperCasedCharacters);
+  if (settings.num) gen = gen.concat(numericCharacters);
+  if (settings.spec) gen = gen.concat(specialCharacters);
+
+  var passphrase = "";
+  
+  for (let index = 0; index < settings.len; index++) {
+    passphrase = passphrase.concat(getRandom(gen));
+  }
+
+  return passphrase;
 }
 
 // Get references to the #generate element
